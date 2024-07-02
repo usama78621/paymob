5f97266c20d00c95567057d0502f1bcfe53df104
usama78621 <usamaijaz912@gmail.com>
2024-07-02 20:04:51 +0500

    psuh code

--- app/(root)/payment-result/page.tsx
+++ app/(root)/payment-result/page.tsx
@@ -1,36 +1,18 @@
 "use client";
 import dynamic from 'next/dynamic';
-import { useRouter } from 'next/router';
+import { useParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
-const PaymentResult = () => {
-    const router = useRouter();
-    const { status, transaction_id, amount } = router.query;
-    const [message, setMessage] = useState('');
+export default function PaymentResult() {
+  const params = useParams()
+  console.log(params)
 
-    useEffect(() => {
-        if (router.isReady) {
-            fetch(`/api/payment-result?status=${status}&transaction_id=${transaction_id}&amount=${amount}`)
-                .then(response => response.json())
-                .then(data => {
-                    console.log(data, 'klfaseknfnjsjn')
-                    setMessage(data.message);
-                });
-        }
-    }, [router.isReady, status, transaction_id, amount]);
 
-    if (!router.isReady) {
-        return <p>Loading...</p>;
-    }
-
-    return (
-        <div>
-            <h1>{message}</h1>
-            {transaction_id && <p>Transaction ID: {transaction_id}</p>}
-            {amount && <p>Amount: {amount}</p>}
-        </div>
-    );
+  return (
+    <div>
+      <p>Transaction ID: </p>
+    </div>
+  );
 };
 
-// Dynamically import the component to ensure client-side rendering
-export default dynamic(() => Promise.resolve(PaymentResult), { ssr: false });
+
