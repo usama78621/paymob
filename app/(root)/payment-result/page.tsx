28624eb19ed0daafd806001a98b3343a175ad5e1
usama78621 <usamaijaz912@gmail.com>
2024-07-02 20:07:36 +0500

    psuh

--- app/(root)/payment-result/page.tsx
+++ app/(root)/payment-result/page.tsx
@@ -1,7 +1,5 @@
 "use client";
-import dynamic from 'next/dynamic';
 import { useParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
 
 export default function PaymentResult() {
   const params = useParams()
